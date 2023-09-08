585e8f338390c065d8efef43e0d0a3cd4990c097
alsir <alsiradil0099@gmail.com>
2023-09-08 16:26:57 -0700

    .....

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *  * main - Print the alphabet in lowercase but in reverse
+ *   * Return: Always 0 (succed).
+ *    */
+int main(void)
+{
+		char ch = 'z';
+
+			while (ch >= 'a')
+					{
+								putchar(ch--);
+									}
+				putchar(\n')	return (0);}';
+
+
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *  * main - Print the numbers of base 16 in lowercase.
+ *   * Return: Always 0 (succed).
+ *    */
+int main(void)
+{
+		char ch = 48;
+
+			while (ch <= 57)
+					{
+								putchar(ch++);
+									}
+				ch = 97;
+
+					while (ch <= 102)
+							{
+										putchar(ch++);
+											}
+						putchar('\n');
+							return (0);
+}
