7491408f2315a66181f114767c10db0b69125c88
Alsir Hamory <41924833+alsir@users.noreply.github.com>
2023-09-15 02:47:25 +0200

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- *  * print_numbers - Print the numbers, from 0 to 9, followed by a new line
- *   * Return: Always void.
- *    */
+* print_numbers - Print the numbers, from 0 to 9, followed by a new line
+* Return: Always void.
+*/
 void print_numbers(void)
 {
-		int i;
-
-			for (i = 48; i <= 57; i++)
-					{
-								_putchar(i);
-									}
-				_putchar('\n');
+	int i;
+	
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
