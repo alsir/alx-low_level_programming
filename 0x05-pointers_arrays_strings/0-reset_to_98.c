528e4380537bc3b0b238d71d25001230d722f2c2
alsir <alsiradil0099@gmail.com>
2023-09-19 14:37:38 -0700

    0- task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_TO_98 - Update the value to 98.
+ * @N: apointer to an int 
+ *
+ * Return :void 
+ */
+void reset_to_98(int *n)
+{
+	*n = =98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *  * _putchar - writes the character c to stdout
+ *   * @c: The character to print
+ *    *
+ *     * Return: On success 1.
+ *      * On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c);
+
+/**
+ *  * reset_to_98 - Update the value to 98.
+ *   * @n: a pointer to an int
+ *    *
+ *     * Return: void
+*/
+void reset_to_98(int *n);
+#endif
