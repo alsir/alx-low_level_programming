00432ab339a15823d9d7914ccf8266879021eb91
alsir <alsiradil0099@gmail.com>
2023-09-19 15:44:45 -0700

    main.h update

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -17,4 +17,84 @@ int _putchar(char c);
  * Return: void
  */
 void reset_to_98(int *n);
+/**
+ * swap_int - Swap the values of two integers
+ * @a: The fisrt element to be swapped
+ * @b: The second element to be swapped
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b);
+
+/**
+ * _strlen - Returns the length of the string
+ * @s: The string to calculate its length
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s);
+
+/**
+ *_puts - Print a string, followed by a new line, to stdout
+ * @str: The string to be printed
+ * Return: void
+ */
+void _puts(char *str);
+
+/**
+ * print_rev - Print a string, in reverse
+ * followed by a new line, to stdout
+ * @s: The string to be printed
+ * Return: void
+ */
+void print_rev(char *s);
+
+/**
+ * rev_string - Reverse a string
+ * @s: The string to be reversed
+ * Return: void
+ */
+void rev_string(char *s);
+
+/**
+ * puts2 - Print every other character of a string
+ * @str: The string to print from it
+ * Return: void
+ */
+void puts2(char *str);
+
+/**
+ * puts_half - Print half of the string
+ * @str: The string to print half of it
+ *
+ * Return: vod
+ */
+void puts_half(char *str);
+
+/**
+ * print_array - Print n elements of an array of integers
+ * @a: The array of integers
+ * @n: The number of elements to be printed
+ * 
+ * Return: void
+ */
+void print_array(int *a, int n);
+
+/**
+ * _strcpy - Copy a string in one variable into another variable
+ * @src: The string to be copied
+ * @dest: The destination of the copied string
+ *
+ *Return: The pointer to the destination.
+ */
+char *_strcpy(char *dest, char *src);
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The integer value of the converted string.
+ */
+int _atoi(char *s);
+
 #endif
