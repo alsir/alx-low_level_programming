db58f1635d7a7c45b1b2a182d32b300cae4e7ae0
Alsir Hamory <41924833+alsir@users.noreply.github.com>
2023-09-19 23:39:49 +0200

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,19 +2,19 @@
 #define MAIN_H
 
 /**
- *  * _putchar - writes the character c to stdout
- *   * @c: The character to print
- *    *
- *     * Return: On success 1.
- *      * On error, -1 is returned, and errno is set appropriately.
-*/
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c);
 
 /**
- *  * reset_to_98 - Update the value to 98.
- *   * @n: a pointer to an int
- *    *
- *     * Return: void
-*/
+ * reset_to_98 - Update the value to 98.
+ * @n: a pointer to an int
+ *
+ * Return: void
+ */
 void reset_to_98(int *n);
 #endif
